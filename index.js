2756f27ad1c2ab79a70b911a043c72fa0d0c5071
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 14:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-let pageTemplate = _.template(document.getElementById("page-template").innerHTML)
-let postTemplate = _.template(document.getElementById("post-template").innerHTML)
-let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
+
+function createPost() {
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML)
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML)
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
 
 
-let postTitle = document.getElementById("postTitle").value;
-let postAuthor = document.getElementById("postAuthor").value;
-let post = document.getElementById("postBody").value;
+  let postTitle = document.getElementById("postTitle").value;
+  let postAuthor = document.getElementById("postAuthor").value;
+  let post = document.getElementById("postBody").value;
 
-document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
-let commentsSection = commentsTemplate();
-let postElement = document.getElementById("post");
+  let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  let commentsSection = commentsTemplate();
+  let postElement = document.getElementById("post");
 
-postElement.innerHTML = blogSection;
-postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
 
 function postComment() {
   var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
