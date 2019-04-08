88d7a12f1a1204c37fbabc4621b915891fb1fcb4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 14:12:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const pageTemplate = _.template(document.getElementById("page-element").innerHTML)
-const postTemplate = _.template(document.getElementById("post-element").innerHTML)
-const commentsTemplate = _.template(document.getElementById("comments-element").innerHTML)
+let pageTemplate = _.template(document.getElementById("page-element").innerHTML)
+let postTemplate = _.template(document.getElementById("post-element").innerHTML)
+let commentsTemplate = _.template(document.getElementById("comments-element").innerHTML)
+
+
+let postTitle = document.getElementById("postTitle").value;
+let postAuthor = document.getElementById("postAuthor").value;
+let post = document.getElementById("postBody").value;
+
+document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+let blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+let commentsSection = commentsTemplate();
+let postElement = document.getElementById("post");
+
+postElement.innerHTML = blogSection;
+postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+}
